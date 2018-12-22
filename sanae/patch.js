dac0da22f18639257d5dec88674d5987a82f8f76
MoriyaFaith <46070717+MoriyaFaith@users.noreply.github.com>
2018-12-22 11:19:39 -0500

    Update patch.js

--- sanae/patch.js
+++ sanae/patch.js
@@ -2,10 +2,6 @@
     "id": "sanae",
     "title": "Sanae's Adventures in Gensokyo",
     "dependencies": [
-        "nmlgc/base_tsa",
-        "nmlgc/base_tasofro",
-        "nmlgc/script_latin",
-        "nmlgc/western_name_order"
 		"thpatch/lang_en"
     ],
     "update": true,
