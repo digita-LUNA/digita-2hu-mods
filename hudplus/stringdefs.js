f3c2e0c2939ee3552b1dbe931a65d67dc5ac5e3d
MoriyaFaith <46070717+MoriyaFaith@users.noreply.github.com>
2018-12-22 14:05:00 -0500

    Added TH14 Sprites for Sakuya

--- hudplus/stringdefs.js
+++ hudplus/stringdefs.js
@@ -1,4 +1,5 @@
 {
     "th07 Bomb Reimu A unfocused": "Spirit Sign \"Fantasy Orb\"",
-    "th07 Bomb Reimu A focused": "Spirit Sign \"Fantasy Seal\""
+    "th07 Bomb Reimu A focused": "Spirit Sign \"Fantasy Seal\"",
+	"th07 Bomb Marisa B unfocused": "Love Sign \"Whirling Laser\""
 }
