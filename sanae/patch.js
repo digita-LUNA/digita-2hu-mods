fc50453ae0bd13363f2833e4603cfeb84d519d65
MoriyaFaith <46070717+MoriyaFaith@users.noreply.github.com>
2018-12-22 10:12:45 -0500

    Added support for Touhou 7

---
+++ sanae/patch.js
@@ -0,0 +1,18 @@
+{
+    "id": "sanae",
+    "title": "Sanae's Adventures in Gensokyo",
+    "dependencies": [
+        "nmlgc/base_tsa",
+        "nmlgc/base_tasofro",
+        "nmlgc/script_latin",
+        "nmlgc/western_name_order"
+		"thpatch/lang_en"
+    ],
+    "update": true,
+    "servers": [
+        "https://srv.thpatch.net/lang_en/",
+        "http://srv.thpatch.net/lang_en/",
+        "https://thcrap.nmlgc.net/repos/thpatch/lang_en/",
+        "http://thcrap.nmlgc.net/repos/thpatch/lang_en/"
+    ]
+}
