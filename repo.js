35d3272eb80c367e67bd93a0af83a617ac688ecd
MoriyaFaith <46070717+MoriyaFaith@users.noreply.github.com>
2018-12-22 11:16:23 -0500

    First Release?

--- repo.js
+++ repo.js
@@ -1,8 +1,11 @@
 {
-	"contact": "mrorigami03@gmail.com",
-	"id": "MoriyaFaith",
-	"patches": {
-		"sanae": "Sanae Expansion Mod (English)\n                          Only works for PCB (for now)."
-	},
-	"title": "Moriya Faith's Touhou Mods"
+    "contact": "@MoriyaFaith#9287",
+    "id": "MoriyaFaith",
+    "servers": [
+        "https://thcrap.nmlgc.net/repos/MoriyaFaith/",
+        "http://thcrap.nmlgc.net/repos/MoriyaFaith/",
+        "https://mirrors.thpatch.net/MoriyaFaith/",
+        "http://mirrors.thpatch.net/MoriyaFaith/"
+    ],
+    "title": "The Modder from Moriya Shrine"
 }
--- sanae/patch.js
+++ sanae/patch.js
@@ -9,5 +9,10 @@
 		"thpatch/lang_en"
     ],
     "update": true,
+    "servers": [
+        "https://thcrap.nmlgc.net/repos/MoriyaFaith/",
+        "http://thcrap.nmlgc.net/repos/MoriyaFaith/",
+        "https://mirrors.thpatch.net/MoriyaFaith/",
+        "http://mirrors.thpatch.net/MoriyaFaith/"
     ]
 }
