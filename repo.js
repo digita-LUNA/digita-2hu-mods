0942ae2d15934cb3bd8531949d30a57f91d390ec
MoriyaFaith <46070717+MoriyaFaith@users.noreply.github.com>
2018-12-22 10:37:27 -0500

    Update repo.js

--- repo.js
+++ repo.js
@@ -1,5 +1,8 @@
 {
 	"contact": "mrorigami03@gmail.com",
 	"id": "MoriyaFaith",
+	"patches": {
+		"sanae": "Sanae Expansion Mod (English)\n                          Only works for PCB (for now)."
+	},
 	"title": "Moriya Faith's Touhou Mods"
 }
