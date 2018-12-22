dd0ab84905740d0773e03f127bf3662a255741b9
MoriyaFaith <46070717+MoriyaFaith@users.noreply.github.com>
2018-12-22 11:32:58 -0500

    Update stringdefs.js

--- sanae/stringdefs.js
+++ sanae/stringdefs.js
@@ -2,6 +2,6 @@
     "th07 Bomb Sakuya A unfocused": "Frog Sign \"Wily Toad\"",
     "th07 Bomb Sakuya A focused": "Frog Sign \"Unload the Toad\"",
     "th07 Bomb Sakuya B unfocused": "Esoterica \"Gray Thaumaturgy\"",
-    "th07 Bomb Sakuya B focused": "Esoterica \"Forgotten Ritual\"",
+    "th07 Bomb Sakuya B focused": "Esoterica \"Forgotten Ritual\""
     
 }
