2c46ed04ce4149f384351577f612cf3c2b97a786
MoriyaFaith <46070717+MoriyaFaith@users.noreply.github.com>
2018-12-22 10:26:09 -0500

    Update patch.js

--- sanae/patch.js
+++ sanae/patch.js
@@ -9,10 +9,5 @@
 		"thpatch/lang_en"
     ],
     "update": true,
-    "servers": [
-        "https://srv.thpatch.net/lang_en/",
-        "http://srv.thpatch.net/lang_en/",
-        "https://thcrap.nmlgc.net/repos/thpatch/lang_en/",
-        "http://thcrap.nmlgc.net/repos/thpatch/lang_en/"
     ]
 }
