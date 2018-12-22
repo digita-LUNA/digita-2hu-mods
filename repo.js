9cababb0ddf429aa19c9caffe18e56996f1ad9bd
MoriyaFaith <46070717+MoriyaFaith@users.noreply.github.com>
2018-12-22 11:30:37 -0500

    Fixed things

--- repo.js
+++ repo.js
@@ -1,14 +1,11 @@
 {
-    "id": "sanae",
-    "title": "Sanae's Adventures in Gensokyo",
-    "dependencies": [
-		"thpatch/lang_en"
-    ],
-    "update": true,
+    "contact": "@MoriyaFaith#9287",
+    "id": "MoriyaFaith",
     "servers": [
         "https://thcrap.nmlgc.net/repos/MoriyaFaith/",
         "http://thcrap.nmlgc.net/repos/MoriyaFaith/",
         "https://mirrors.thpatch.net/MoriyaFaith/",
         "http://mirrors.thpatch.net/MoriyaFaith/"
-    ]
+    ],
+    "title": "The Modder from Moriya Shrine"
 }
---
+++ update.bat
@@ -0,0 +1,2 @@
+C:\Users\Josh\AppData\Local\Programs\Python\Python37-32\python.exe ..\scripts\repo_update.py
+pause
