ac411fd343f0b09787744a53f6206877177dda8a
MoriyaFaith <46070717+MoriyaFaith@users.noreply.github.com>
2018-12-23 13:02:58 -0500

    Added Full Support for Touhou 8

--- sanae/stringdefs.js
+++ sanae/stringdefs.js
@@ -5,6 +5,8 @@
     "th07 Bomb Sakuya B focused": "Esoterica \"Forgotten Ritual\"",
 	"th08 Bomb Sakuya Last": "Miracle \"God's Wind\"",
     "th08 Bomb Remilia": "Native God \"Kero-Chan Braves the Storm\"",
-    "th08 Bomb Remilia Last": "Native God \"Moriya God\""
+    "th08 Bomb Remilia Last": "Native God \"Moriya God\"",
+	"th07 Stats SakuyaA": "Sanae Kochiya (Frog)",
+    "th07 Stats SakuyaB": "Sanae Kochiya (Esoterica)"
     
 }
