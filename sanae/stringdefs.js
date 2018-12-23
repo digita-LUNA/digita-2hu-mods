5ae637d6d739d527dd76dfc2f3205014081e9392
MoriyaFaith <46070717+MoriyaFaith@users.noreply.github.com>
2018-12-22 21:17:02 -0500

    Added Support for Touhou 8
    Basic Support added for Touhou 8.
    Still needs Dialogue and Loading Screens.

--- sanae/stringdefs.js
+++ sanae/stringdefs.js
@@ -2,6 +2,9 @@
     "th07 Bomb Sakuya A unfocused": "Frog Sign \"Wily Toad\"",
     "th07 Bomb Sakuya A focused": "Frog Sign \"Unload the Toad\"",
     "th07 Bomb Sakuya B unfocused": "Esoterica \"Gray Thaumaturgy\"",
-    "th07 Bomb Sakuya B focused": "Esoterica \"Forgotten Ritual\""
+    "th07 Bomb Sakuya B focused": "Esoterica \"Forgotten Ritual\"",
+	"th08 Bomb Sakuya Last": "Miracle \"God's Wind\"",
+    "th08 Bomb Remilia": "Native God \"Kero-Chan Braves the Storm\"",
+    "th08 Bomb Remilia Last": "Native God \"Moriya God\""
     
 }
