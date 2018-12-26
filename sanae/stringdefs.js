19a818fd5d59aa095b3778697899922cded65c09
MoriyaFaith <46070717+MoriyaFaith@users.noreply.github.com>
2018-12-26 08:03:25 -0500

    Added Mima over Alice to TH8 and Nitori in 11

--- sanae/stringdefs.js
+++ sanae/stringdefs.js
@@ -3,6 +3,8 @@
     "th07 Bomb Sakuya A focused": "Frog Sign \"Unload the Toad\"",
     "th07 Bomb Sakuya B unfocused": "Esoterica \"Gray Thaumaturgy\"",
     "th07 Bomb Sakuya B focused": "Esoterica \"Forgotten Ritual\"",
+	"th08 Bomb Alice": "Celestial Sign \"Orreries Sun\"",
+    "th08 Bomb Alice Last": "Mima's Universe \"Reincarnation\"",
 	"th08 Bomb Sakuya Last": "Miracle \"God's Wind\"",
     "th08 Bomb Remilia": "Native God \"Kero-Chan Braves the Storm\"",
     "th08 Bomb Remilia Last": "Native God \"Moriya God\"",
