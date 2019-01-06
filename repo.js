8be432b0e03cbd7493e59379dc38945aafdf61b5
MoriyaFaith <46070717+MoriyaFaith@users.noreply.github.com>
2019-01-05 22:28:59 -0500

    fyv

--- repo.js
+++ repo.js
@@ -2,9 +2,9 @@
 	"contact": "@MoriyaFaith#9287",
 	"id": "MoriyaFaith",
 	"patches": {
+		"4kids-expansion": "Character Expansion Pack for the Mysteryland Project, Featuring Jonathan Johnson.",
 		"hudplus": "Better Character Select, Loading Screens, and Player Sprites for Touhou 7 & 8",
-		"sanae": "Sanae (And Suwako) in Touhou 7 & 8, 9 and 11 are unfinished, but also included.",
-		"4kids-expansion": "Character Expansion Pack for the Mysteryland Project, Featuring Jonathan Johnson."
+		"sanae": "Sanae (And Suwako) in Touhou 7 & 8, 9 and 11 are unfinished, but also included."
 	},
 	"servers": [
 		"https://thcrap.nmlgc.net/repos/MoriyaFaith/",
