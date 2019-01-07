e291435ad0cc4f242a0402007956a5fcf3e126e3
MoriyaFaith <46070717+MoriyaFaith@users.noreply.github.com>
2019-01-07 17:19:18 -0500

    commit the change large

--- 4kids-expansion/patch.js
+++ 4kids-expansion/patch.js
@@ -9,6 +9,6 @@
 		"https://mirrors.thpatch.net/MoriyaFaith/4kids-expansion/",
 		"http://mirrors.thpatch.net/MoriyaFaith/4kids-expansion/"
 	],
-	"title": "Character Expansion Pack for the Mysteryland Project, Featuring Jonathan Johnson.",
+	"title": "Character Expansion Pack for the Mysteryland Project, Featuring Emmah Johnson.",
 	"update": true
 }
--- repo.js
+++ repo.js
@@ -2,7 +2,7 @@
 	"contact": "@MoriyaFaith#9287",
 	"id": "MoriyaFaith",
 	"patches": {
-		"4kids-expansion": "Character Expansion Pack for the Mysteryland Project, Featuring Jonathan Johnson.",
+		"4kids-expansion": "Character Expansion Pack for the Mysteryland Project, Featuring Emmah Johnson.",
 		"hudplus": "Better Character Select, Loading Screens, and Player Sprites for Touhou 7 & 8",
 		"sanae": "Sanae (And Suwako) in Touhou 7 & 8, 9 and 11 are unfinished, but also included."
 	},
