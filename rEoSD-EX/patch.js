e844b2924e41eab4f5eb66667760f8b93515aa74
MoriyaFaith <46070717+MoriyaFaith@users.noreply.github.com>
2019-01-22 17:00:43 -0500

    Added RE:EoSD EX

---
+++ rEoSD-EX/patch.js
@@ -0,0 +1,14 @@
+{
+	"dependencies": [
+		"thpatch/lang_en",
+	],
+	"id": "rEoSD-EX",
+	"servers": [
+		"https://thcrap.nmlgc.net/repos/MoriyaFaith/rEoSD-EX/",
+		"http://thcrap.nmlgc.net/repos/MoriyaFaith/rEoSD-EX/",
+		"https://mirrors.thpatch.net/MoriyaFaith/rEoSD-EX/",
+		"http://mirrors.thpatch.net/MoriyaFaith/rEoSD-EX/"
+	],
+	"title": "Adds extra goodies to the rEoSD Project, including Magic Missile and Rin Satsuki.",
+	"update": true
+}
--- repo.js
+++ repo.js
@@ -5,7 +5,7 @@
 		"betterfonts": "More Visually Appealing Fonts",
 		"hudplus": "Better Character Select, Loading Screens, and Player Sprites for Touhou 7 & 8",
 		"sanae": "Sanae (And Suwako) in Touhou 7 & 8, 9 and 11 are unfinished, but also included.",
-		"PCBweapons": "(DDC) Replaces Weapons with those from PCB. (Having trouble porting Sakuya's Shots. any help is appreciated!)"
+		"rEoSD-EX": "Adds extra goodies to the rEoSD Project, including Magic Missile and Rin Satsuki."
 	},
 	"servers": [
 		"https://thcrap.nmlgc.net/repos/MoriyaFaith/",
