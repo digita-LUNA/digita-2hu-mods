cabda8131321d9964dd488f350dba5ea51a920fb
MoriyaFaith <46070717+MoriyaFaith@users.noreply.github.com>
2019-01-22 16:40:32 -0500

    changed illusionsign to PCBweapons

---
+++ PCBweapons/patch.js
@@ -0,0 +1,15 @@
+{
+	"dependencies": [
+		"thpatch/lang_en"
+		
+	],
+	"id": "PCBweapons",
+	"servers": [
+		"https://thcrap.nmlgc.net/repos/MoriyaFaith/PCBweapons/",
+		"http://thcrap.nmlgc.net/repos/MoriyaFaith/PCBweapons/",
+		"https://mirrors.thpatch.net/MoriyaFaith/PCBweapons/",
+		"http://mirrors.thpatch.net/MoriyaFaith/PCBweapons/"
+	],
+	"title": "(DDC) Replaces Weapons with those from PCB. (Having trouble porting Sakuya's Shots. any help is appreciated!)",
+	"update": true
+}
--- repo.js
+++ repo.js
@@ -5,7 +5,7 @@
 		"betterfonts": "More Visually Appealing Fonts",
 		"hudplus": "Better Character Select, Loading Screens, and Player Sprites for Touhou 7 & 8",
 		"sanae": "Sanae (And Suwako) in Touhou 7 & 8, 9 and 11 are unfinished, but also included.",
-		"illusionsign": "(DDC) Replaces SakuyaA with Illusion Sign, from PCB."
+		"PCBweapons": "(DDC) Replaces Weapons with those from PCB. (Having trouble porting Sakuya's Shots. any help is appreciated!)"
 	},
 	"servers": [
 		"https://thcrap.nmlgc.net/repos/MoriyaFaith/",
