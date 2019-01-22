884e123aa564d397418d69f9a681cc447d5f733b
MoriyaFaith <46070717+MoriyaFaith@users.noreply.github.com>
2019-01-22 16:02:12 -0500

    Added Illusion Sign to Repo

--- repo.js
+++ repo.js
@@ -4,7 +4,8 @@
 	"patches": {
 		"betterfonts": "More Visually Appealing Fonts",
 		"hudplus": "Better Character Select, Loading Screens, and Player Sprites for Touhou 7 & 8",
-		"sanae": "Sanae (And Suwako) in Touhou 7 & 8, 9 and 11 are unfinished, but also included."
+		"sanae": "Sanae (And Suwako) in Touhou 7 & 8, 9 and 11 are unfinished, but also included.",
+		"illusionsign": "(DDC) Replaces SakuyaA with Illusion Sign, from PCB."
 	},
 	"servers": [
 		"https://thcrap.nmlgc.net/repos/MoriyaFaith/",
