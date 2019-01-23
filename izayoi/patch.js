f234ad8087776ef354a68de2d8f0a96fc78d8620
MoriyaFaith <46070717+MoriyaFaith@users.noreply.github.com>
2019-01-23 13:59:00 -0500

    Turned th16 sakuya (previously in sanae) into the Izayoi Project

---
+++ izayoi/patch.js
@@ -0,0 +1,16 @@
+{
+	"dependencies": [
+		"MoriyaFaith/hudplus",
+		"thpatch/lang_en"
+		
+	],
+	"id": "izayoi",
+	"servers": [
+		"https://thcrap.nmlgc.net/repos/MoriyaFaith/izayoi/",
+		"http://thcrap.nmlgc.net/repos/MoriyaFaith/izayoi/",
+		"https://mirrors.thpatch.net/MoriyaFaith/izayoi/",
+		"http://mirrors.thpatch.net/MoriyaFaith/izayoi/"
+	],
+	"title": "(TH16) the Izayoi Project.",
+	"update": true
+}
