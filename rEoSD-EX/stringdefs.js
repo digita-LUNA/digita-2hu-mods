2308b134b03327fd2a07add54587d93e02adaaba
MoriyaFaith <46070717+MoriyaFaith@users.noreply.github.com>
2019-01-22 20:15:29 -0500

    (rEoSD EX) Added Music and proper title bar name

---
+++ rEoSD-EX/stringdefs.js
@@ -0,0 +1,4 @@
+{
+	"th12": "Touhou Koumakyou - The Embodiment of Scarlet Devil",
+	"th12 JP": "Touhou Koumakyou"
+}
---
+++ rEoSD-EX/themes.js
@@ -0,0 +1,19 @@
+{
+    "th12_01": "A Dream More Scarlet than Red",
+    "th12_02": "A Soul as Red as a Ground Cherry",
+    "th12_03": "Apparitions Stalk the Night",
+    "th12_04": "Lunate Elf",
+    "th12_05": "Tomboyish Girl in Love",
+    "th12_06": "Shanghai Teahouse ~ Chinese Tea",
+    "th12_07": "Shanghai Alice of Meiji 17",
+    "th12_08": "Voile, the Magic Library",
+    "th12_09": "Locked Girl ~ The Girl's Secret Room",
+    "th12_10": "The Maid and the Pocket Watch of Blood",
+    "th12_11": "Lunar Clock ~ Luna Dial",
+    "th12_12": "The Young Descendant of Țepeș",
+    "th12_13": "Septette for a Dead Princess",
+    "th12_14": "The Centennial Festival for Magical Girls",
+    "th12_15": "U.N. Owen was Her?",
+    "th12_16": "An Eternity More Transient than Scarlet",
+    "th12_17": "Scarlet Tower ~ Eastern Dream..."
+}
