52986a4c261eae63e449823249a345925631b4aa
MoriyaFaith <46070717+MoriyaFaith@users.noreply.github.com>
2019-01-22 22:58:28 -0500

    Update repo.js

--- repo.js
+++ repo.js
@@ -1,12 +1,6 @@
 {
 	"contact": "@MoriyaFaith#9287",
 	"id": "MoriyaFaith",
-	"patches": {
-		"betterfonts": "More Visually Appealing Fonts",
-		"hudplus": "Huge graphical updates to Touhous 6, 7, and 8.",
-		"sanae": "Sanae (And Suwako) in Touhou 7 & 8, 9 and 11 are unfinished, but also included.",
-		"rEoSD-EX": "Adds extra goodies to the rEoSD Project, including Magic Missile and Rin Satsuki.\n                          rEoSD originally by dass7. Many assets, like MarisaB's shot, were made by Priw8."
-	},
 	"servers": [
 		"https://thcrap.nmlgc.net/repos/MoriyaFaith/",
 		"http://thcrap.nmlgc.net/repos/MoriyaFaith/",
