3b12194fca7eb8168432414e893e9549f67cfc3f
MoriyaFaith <46070717+MoriyaFaith@users.noreply.github.com>
2020-11-01 20:33:09 -0500

    Commented out a couple unfinished projects, updated hudplus
    I'm back bitches

--- repo.js
+++ repo.js
@@ -1,11 +1,17 @@
 {
-	"contact": "@MoriyaFaith#9287",
-	"id": "MoriyaFaith",
-	"servers": [
-		"https://thcrap.nmlgc.net/repos/MoriyaFaith/",
-		"http://thcrap.nmlgc.net/repos/MoriyaFaith/",
-		"https://mirrors.thpatch.net/MoriyaFaith/",
-		"http://mirrors.thpatch.net/MoriyaFaith/"
-	],
-	"title": "MoriyaFaith's Improvement and Character Mods"
+    "id": "MoriyaFaith",
+    "title": "@MoriyaFaith#9287",
+    "servers": [
+        "https://thcrap.nmlgc.net/repos/MoriyaFaith/",
+        "http://thcrap.nmlgc.net/repos/MoriyaFaith/",
+        "https://mirrors.thpatch.net/MoriyaFaith/",
+        "http://mirrors.thpatch.net/MoriyaFaith/"
+    ],
+    "patches": {
+        "hudplus": "Huge graphical updates to Touhous 6, 7, and 8.",
+        "izayoi": "(TH16) the Izayoi Project.",
+        //"rEoSD-EX": "Adds extra goodies to the rEoSD Project, including Magic Missile and Rin Satsuki.\n rEoSD originally by dass7. Many assets, like MarisaB's shot, were made by Priw8.",
+        //"rEoSD-EX_alphes": "Alphes patch for EoSD-EX",
+        "sanae": "(TH7, 8, 9 [WIP], 11[WIP], 16) the Kochiya Project."
+    }
 }
