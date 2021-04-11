0fea8c01613abe6b4f2cae6519c715e3d0b7cfc6
MoriyaFaith <46070717+MoriyaFaith@users.noreply.github.com>
2021-04-10 22:34:07 -0400

    Update repo.js
    Fixed comments breaking THCRAP

--- repo.js
+++ repo.js
@@ -13,5 +13,3 @@
         "sanae": "(TH7, 8, 9 [WIP], 11[WIP], 16) the Kochiya Project."
     }
 }
-        //"rEoSD-EX": "Adds extra goodies to the rEoSD Project, including Magic Missile and Rin Satsuki.\n rEoSD originally by dass7. Many assets, like MarisaB's shot, were made by Priw8.",
-        //"rEoSD-EX_alphes": "Alphes patch for EoSD-EX",
