b30e86bdd1fcb225dc05a7be92afb0c66a586c33
MoriyaFaith <46070717+digita-LUNA@users.noreply.github.com>
2025-08-28 18:56:38 -0400

    Update patch.js

--- gfx-modern/patch.js
+++ gfx-modern/patch.js
@@ -1,16 +1,17 @@
 {
 	"dependencies": [
 		"thpatch/lang_en"
+		"digita-2hu-mods/gfxplus/"
 	],
 	"fonts": {
 		"obelisque.otf": true
 	},
 	"id": "gfx-modern",
 	"servers": [
-		"https://thcrap.nmlgc.net/repos/digita-2hu-mods/gfxplus/",
-		"http://thcrap.nmlgc.net/repos/digita-2hu-mods/gfxplus/",
-		"https://mirrors.thpatch.net/digita-2hu-mods/gfxplus/",
-		"http://mirrors.thpatch.net/digita-2hu-mods/gfxplus/"
+		"https://thcrap.nmlgc.net/repos/digita-2hu-mods/gfx-modern/",
+		"http://thcrap.nmlgc.net/repos/digita-2hu-mods/gfx-modern/",
+		"https://mirrors.thpatch.net/digita-2hu-mods/gfx-modern/",
+		"http://mirrors.thpatch.net/digita-2hu-mods/gfx-modern/"
 	],
 	"title": "Version of gfxplus that uses TH10+ Sprites.",
 	"update": true
