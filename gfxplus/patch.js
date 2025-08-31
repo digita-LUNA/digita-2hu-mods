182561f19ababfd6af642100b814ed1693600d76
MoriyaFaith <46070717+digita-LUNA@users.noreply.github.com>
2025-08-31 17:15:41 -0400

    disable gfxmodern, remove now-unneeded files

--- gfxplus/patch.js
+++ gfxplus/patch.js
@@ -1,17 +1,17 @@
 {
-	"dependencies": [
-		"thpatch/lang_en"
-	],
-	"fonts": {
-		"obelisque.otf": true
-	},
-	"id": "gfxplus",
-	"servers": [
-		"https://thcrap.nmlgc.net/repos/digita-2hu-mods/gfxplus/",
-		"http://thcrap.nmlgc.net/repos/digita-2hu-mods/gfxplus/",
-		"https://mirrors.thpatch.net/digita-2hu-mods/gfxplus/",
-		"http://mirrors.thpatch.net/digita-2hu-mods/gfxplus/"
-	],
-	"title": "(TH06, TH07) Major player graphical update. NOT REPLAY COMPATIBLE.",
-	"update": true
+  "dependencies": [
+    "thpatch/lang_en"
+  ],
+  "fonts": {
+    "obelisque.otf": true
+  },
+  "id": "gfxplus",
+  "servers": [
+    "https://thcrap.nmlgc.net/repos/digita-2hu-mods/gfxplus/",
+    "http://thcrap.nmlgc.net/repos/digita-2hu-mods/gfxplus/",
+    "https://mirrors.thpatch.net/digita-2hu-mods/gfxplus/",
+    "http://mirrors.thpatch.net/digita-2hu-mods/gfxplus/"
+  ],
+  "title": "(TH06, TH07) Major player graphical update. NOT REPLAY COMPATIBLE.",
+  "update": true
 }
