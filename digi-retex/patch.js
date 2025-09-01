de7c9eb987472b01b3a9322d7506c54183333fc2
MoriyaFaith <46070717+digita-LUNA@users.noreply.github.com>
2025-09-01 15:12:37 -0400

    significant update to TH06 Menu graphics, rename retexture mod to be less pretentious (gfxplus -> mori-retex)

---
+++ digi-retex/patch.js
@@ -0,0 +1,17 @@
+{
+	"dependencies": [
+		"thpatch/lang_en"
+	],
+	"fonts": {
+		"obelisque.otf": true
+	},
+	"id": "digi-retex",
+	"servers": [
+		"https://thcrap.nmlgc.net/repos/digita-2hu-mods/digi-retex/",
+		"http://thcrap.nmlgc.net/repos/digita-2hu-mods/digi-retex/",
+		"https://mirrors.thpatch.net/digita-2hu-mods/digi-retex/",
+		"http://mirrors.thpatch.net/digita-2hu-mods/digi-retex/"
+	],
+	"title": "(TH06, TH07) Large-scale retexture for older titles. NOT REPLAY COMPATIBLE.",
+	"update": true
+}
--- repo.js
+++ repo.js
@@ -1,16 +1,16 @@
 {
-    "id": "digita-2hu-mods",
-    "title": "digita-LUNA's Improvement and Character Mods",
-    "contact": "@digita_luna",
-    "servers": [
-        "https://thcrap.nmlgc.net/repos/digita-2hu-mods/",
-        "http://thcrap.nmlgc.net/repos/digita-2hu-mods/",
-        "https://mirrors.thpatch.net/digita-2hu-mods/",
-        "http://mirrors.thpatch.net/digita-2hu-mods/"
-    ],
-    "patches": {
-        "gfxplus": "(TH06, TH07) Major player graphical update. NOT REPLAY COMPATIBLE.",
-        "izayoi": "(TH16) the Izayoi Project. Adds Sakuya Izayoi to Touhou 16.",
-        "sanae": "(TH7, 8, 9 [WIP], 11[WIP], 16) the Kochiya Project. Adds Sanae to games she doesn't appear in."
-    }
+	"contact": "@digita_luna",
+	"id": "digita-2hu-mods",
+	"patches": {
+		"digi-retex": "(TH06, TH07) Major graphical update. NOT REPLAY COMPATIBLE.",
+		"izayoi": "(TH16) the Izayoi Project. Adds Sakuya Izayoi to Touhou 16.",
+		"sanae": "(TH7, 8, 9 [WIP], 11[WIP], 16) the Kochiya Project. Adds Sanae to games she doesn't appear in."
+	},
+	"servers": [
+		"https://thcrap.nmlgc.net/repos/digita-2hu-mods/",
+		"http://thcrap.nmlgc.net/repos/digita-2hu-mods/",
+		"https://mirrors.thpatch.net/digita-2hu-mods/",
+		"http://mirrors.thpatch.net/digita-2hu-mods/"
+	],
+	"title": "digita-LUNA's Improvement and Character Mods"
 }
